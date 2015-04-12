277b1f115cdcf57c93cc8ce4c54ab41f09a1f332
skumo <skathimanda@gmail.com>
2015-04-12 09:57:58 +0930

    commit 5

--- HTML/js/astro.js
+++ HTML/js/astro.js
@@ -1,72 +1,75 @@
-
-function foo(){
-        var bloodPressure = 140 / 90;
-        var baseEye = 10;
-        var aerobicWorkCapacity = 32.9;
-        var running = 215;
-        var swimming = 19;
-        var skiRace = 21;
-
-        var userBP = document.getElementById("Criteria1").value;
-        var userEye = document.getElementById("Criteria2").value;
-        var userAWC = document.getElementById("Criteria3").value;
-        var userRun = document.getElementById("Criteria4").value;
-        var userSwim = document.getElementById("Criteria5").value;
-        var userSki = document.getElementById("Criteria16").value;
-
-        var resultBP, resultEye, resultAWC, resultRun, resultSwim, resultSki;
-
-    if (userBP > bloodPressure){
-            resultBP = 100; // Code Red
-        }
-        else{
-              resultBP = 80; // Code Green - Sample data
-        }
-        if (userEye < baseEye){
-            resultEye = 100; // Code Red
-        }
-        else{
-            resultEye = 95; // Code Green
-        }
-
-        if (userAWC < aerobicWorkCapacity){
-            resultAWC = userAWC;
-        }
-        else{
-            resultAWC = 100;
-        }
-        if (userRun < running){
-            resultRun = userRun;
-        }
-        else{
-            resultRun = 100;
-        }
-
-        if (userSwim < swimming){
-            resultSwim = 100;
-        }
-        else{
-            resultSwim = userSwim;
-        }
-
-        if (userSki < skiRace){
-            resultSki = 100;
-        }
-        else{
-            resultSki = userSki;
-        }
-
-
-
-
-}
-
-
-
-
-//Criteria 1 : Eye Sight  20 / 20
-//Criteria 2: Blood Pressure - 140 / 90
-//Criteria 3: Aerobic Work Capacity - Above 32.9 ml\kg\min
-//Criteria 4: Running  - 3 mins 35 Secs - 215 Seconds
-//Criteria 5: Swimming - 19sec - 25 mtrs
-//Criteria 6: Ski Race 5kms - 21mins
+//
+//function foo(){
+//        var bloodPressure = 140 / 90;
+//        var baseEye = 10;
+//        var aerobicWorkCapacity = 32.9;
+//        var running = 215;
+//        var swimming = 19;
+//        var skiRace = 21;
+//
+//        var userBP = document.getElementById("Criteria1").value;
+////        loadLiquidFillGauge("fillgauge1", userBP);
+//        console.log("hey");
+//
+//        var userEye = document.getElementById("Criteria2").value;
+//        var userAWC = document.getElementById("Criteria3").value;
+//        var userRun = document.getElementById("Criteria4").value;
+//        var userSwim = document.getElementById("Criteria5").value;
+//        var userSki = document.getElementById("Criteria16").value;
+//
+//        var resultBP, resultEye, resultAWC, resultRun, resultSwim, resultSki;
+//
+//    if (userBP > bloodPressure){
+//            resultBP = 100; // Code Red
+//        }
+//        else{
+//              resultBP = 80; // Code Green - Sample data
+//        }
+//        if (userEye < baseEye){
+//            resultEye = 100; // Code Red
+//        }
+//        else{
+//            resultEye = 95; // Code Green
+//        }
+//
+//        if (userAWC < aerobicWorkCapacity){
+//            resultAWC = userAWC;
+//        }
+//        else{
+//            resultAWC = 100;
+//        }
+//        if (userRun < running){
+//            resultRun = userRun;
+//        }
+//        else{
+//            resultRun = 100;
+//        }
+//
+//        if (userSwim < swimming){
+//            resultSwim = 100;
+//        }
+//        else{
+//            resultSwim = userSwim;
+//        }
+//
+//        if (userSki < skiRace){
+//            resultSki = 100;
+//        }
+//        else{
+//            resultSki = userSki;
+//        }
+//
+////    loadLiquidFillGauge(document.getElementById("fillguage1"),55)
+//
+//
+//}
+//
+//
+//
+//
+////Criteria 1 : Eye Sight  20 / 20
+////Criteria 2: Blood Pressure - 140 / 90
+////Criteria 3: Aerobic Work Capacity - Above 32.9 ml\kg\min
+////Criteria 4: Running  - 3 mins 35 Secs - 215 Seconds
+////Criteria 5: Swimming - 19sec - 25 mtrs
+////Criteria 6: Ski Race 5kms - 21mins
